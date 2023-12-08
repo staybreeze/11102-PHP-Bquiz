0190cf597ec4b7fee83c3a4220b8dcaaa397423e
mackliu <macklun@gmail.com>
2023-12-08 09:46:12 +0800

    將進站總人數讀出並顯示在網站上

--- back/total.php
+++ back/total.php
@@ -5,7 +5,9 @@
             <tbody>
                 <tr class="yel">
                     <td width="50%">網站標題</td>
-                    <td width="50%"><input type="number" name="total" value="<?=$Total->find(1)['total'];?>"></td>
+                    <td width="50%">
+                        <input type="number" name="total" value="<?=$Total->find(1)['total'];?>">
+                    </td>
                 </tr>
             </tbody>
         </table>
