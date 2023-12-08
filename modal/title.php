eb8d51923bf1c25ff6fc43ffcc804f3965d9f17e
mackliu <macklun@gmail.com>
2023-12-08 13:15:15 +0800

    建立新增資料api檔案add

--- modal/title.php
+++ modal/title.php
@@ -1,6 +1,6 @@
-<h3>新增網站標題圖片<?=$_GET['table'];?></h3>
+<h3>新增網站標題圖片</h3>
 <hr>
-<form action="./api/add_title" method="post" enctype="multipart/form-data">
+<form action="./api/add.php" method="post" enctype="multipart/form-data">
 <table>
     <tr>
         <td>標題區圖片</td>
