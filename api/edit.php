4cb453413cbbb1bd3199e7fdabd57aa149dbe4b7
staybreeze <wunshengliao@gmail.com>
2023-12-10 11:19:25 +0800

    docs:新增註解

--- api/edit.php
+++ api/edit.php
@@ -2,6 +2,7 @@
 
 $table=$_POST['table'];
 $DB=${ucfirst($table)};
+// 從HIDDEN隨POST傳值過來的TABLE要關掉，以便項目跟資料庫對得起來
 unset($_POST['table']);
 
 foreach($_POST['text'] as $id => $text){
@@ -11,8 +12,10 @@ foreach($_POST['text'] as $id => $text){
         $row=$DB->find($id);
         $row['text']=$text;
         if($table=='title'){
+            // 單一指定ID
             $row['sh']=(isset($_POST['sh']) && $_POST['sh']==$id)?1:0;
         }else{
+            // 可以選多個ID
             $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
         }
         $DB->save($row);
