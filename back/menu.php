b6c922a56f6a4fdc509c94060548552aac4ef59a
staybreeze <wunshengliao@gmail.com>
2023-12-11 16:02:08 +0800

    feat:建立編輯次選單頁面

--- back/menu.php
+++ back/menu.php
@@ -30,6 +30,11 @@
                     <td>
                         <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
                     </td>
+        
+         
+                    <td>
+                        <input type="button" value="編輯次選單" onclick="op('#cover','#cvr','./modal/submenu.php?table=<?= $do; ?>&id=<?= $row['id']; ?>')" >
+                    </td>
                 </tr>
                 <input type="hidden" name="id[]" value="<?=$row['id'];?>">
                 <?php
