0999eba2267e729b284abd00ef2eadab6f2d4e1a
staybreeze <wunshengliao@gmail.com>
2023-12-15 09:51:33 +0800

    feat:完成編輯次選單前端畫面及表單設定，設定submenu api程式概況

---
+++ api/submenu.php
@@ -0,0 +1,20 @@
+<?php
+include_once "db.php";
+
+// 先檢查ID，以便確認有主選單
+if(isset($_POST['id'])){
+foreach($_POST['id'] as $idx => $id){
+
+
+}
+}
+
+
+if(isset($_POST['add_text'])){
+    foreach($_POST['add_text'] as $idx => $text){
+
+
+    }
+}
+
+to("../back.php?do=menu");
--- modal/submenu.php
+++ modal/submenu.php
@@ -4,7 +4,7 @@
 <h3>編輯次選單</h3>
 <hr>
 <form action="./api/submenu.php" method="post" enctype="multipart/form-data">
-<table class='cent'>
+<table class='cent' id=sub>
     <tr>
         <td>次選單名稱</td>
         <td>次選單連結網址</td>
@@ -24,15 +24,15 @@
     }
     ?>
     <tr>
-        <td><input type="text" name="text[]" id=""></td>
-        <td><input type="text" name="href[]" id=""></td>
+        <td><input type="text" name="add_text[]" id=""></td>
+        <td><input type="text" name="add_href[]" id=""></td>
     </tr>
 </table>
 <div>
     <input type="hidden" name="table" value="<?=$_GET['table'];?>">
     <input type="submit" value="修改確定">
     <input type="reset" value="重置">
-    <input type="button" value="更多次選單">
+    <input type="button" value="更多次選單"  onclick="more()">
 </div>
 
 </form>
@@ -40,11 +40,11 @@
 
 function more(){
     let item=`<tr>
-                <td><input type="text" name="text[]" id=""></td>
-                <td><input type="text" name="href[]" id=""></td>
+                <td><input type="text" name="add_text[]" id=""></td>
+                <td><input type="text" name="add_href[]" id=""></td>
               </tr>`
 
-    $("#xxx").append(item);
+    $("#sub").append(item);
 
 }
 
