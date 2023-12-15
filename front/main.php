f61a53990ec7628ca35b5a9a21f545273126da91
staybreeze <wunshengliao@gmail.com>
2023-12-15 15:41:44 +0800

    feat:完成彈出視窗內容顯示的設定

--- front/main.php
+++ front/main.php
@@ -40,14 +40,45 @@
     </script>
     <div style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
         <span class="t botli">最新消息區
+
+        <?php
+        if($News->count(['sh'=>1])>5){
+
+            echo '<a href="?do=news" style="float:right">More...</a>';
+        }
+        ?>
+
         </span>
         <ul class="ssaa" style="list-style-type:decimal;">
+            <!-- <li>a</li>
+            <li>b</li>
+            <li>c</li>
+            <li>d</li>
+            <li>e</li> -->
+
+            <?php
+            
+            // ' limit 5'加個空白以便SQL不會判斷錯誤
+            $news=$News->all(['sh'=>1],' limit 5');
+            foreach($news as $n){
+                echo "<li>";
+                echo mb_substr($n['text'],0,20);
+                // 增加一個DIV使文字全部顯現，但同時又使其display:none，以便$(this).children(".all").html()吃得到
+                echo "<div class='all' style='display:none'>";
+                echo $n['text'];
+                echo "</div>";
+                echo "...</li>";
+            }
+            ?>
+
+
         </ul>
         <div id="altt" style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
         <script>
             $(".ssaa li").hover(
                 function() {
-                    $("#altt").html("<pre>" + $(this).children(".all").html() + "</pre>")
+                    //$(this).children(".all").html()
+                    $("#altt").html("<pre>" +$(this).children(".all").html()  + "</pre>")
                     $("#altt").show()
                 }
             )
