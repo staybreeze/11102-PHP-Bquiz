0fccaac5f7113781e9b9f93fb12ab0efafab0887
staybreeze <wunshengliao@gmail.com>
2023-12-18 09:39:09 +0800

    feat(index):完成校園映像區製作

--- index.php
+++ index.php
@@ -56,17 +56,17 @@
 					<span class="t botli">校園映象區</span>
 
 					<?php
-					$imgs=$Image->all(['sh'=>1]);
+					// $imgs=$Image->all(['sh'=>1]);
 
-					foreach($imgs as $idx => $img){
+					// foreach($imgs as $idx => $img){
 					?>
-						<div id="ssaa<?=$idx;?>" class='im'>
-							<img src="./img/<?=$img['img'];?>" style="width:150px;height:103px">
-						</div>
+					<!-- <div id="ssaa<?= $idx; ?>" class='im'>
+							<img src="./img/<?= $img['img']; ?>" style="width:150px;height:103px">
+						</div> -->
 					<?php
-					}
+					// }
 					?>
-						<script>
+					<!-- <script>
 									// 因應程式本身的BUG，nowpage=0改1
                         	var nowpage=1,num=0;
 							function pp(x)
@@ -90,7 +90,46 @@
 
 	// 執行pp(2)
 							pp(2)
-                        </script>
+                        </script> -->
+
+						<!-- 修改BUG後採用的版本 -->
+					<div class="cent" onclick="pp(1)"><img src="./icon/up.jpg" alt=""></div>
+					<?php
+					$imgs = $Image->all(['sh' => 1]);
+
+					foreach ($imgs as $idx => $img) {
+					?>
+						<div id="ssaa<?= $idx; ?>" class='im cent'>
+							<img src="./img/<?= $img['img']; ?>" style="width:150px;height:103px;border:3px solid orange;margin:3px">
+						</div>
+					<?php
+					}
+					?>
+					<div class="cent" onclick="pp(2)"><img src="./icon/dn.jpg" alt=""></div>
+					<script>
+						var nowpage = 1,
+							num = <?= $Image->count(['sh' => 1]); ?>;
+
+						function pp(x) {
+							var s, t;
+							if (x == 1 && nowpage - 1 >= 0) {
+								nowpage--;
+							}
+							if (x == 2 && (nowpage + 1) <= num * 1 - 3) {
+								nowpage++;
+							}
+
+							$(".im").hide()
+							for (s = 0; s <= 2; s++) {
+								t = s * 1 + nowpage * 1;
+								$("#ssaa" + t).show()
+
+							}
+						}
+
+
+						pp(2)
+					</script>
 				</div>
 			</div>
 		</div>
