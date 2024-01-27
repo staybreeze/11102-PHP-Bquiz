206f2ff50b6145fad23713c135072e2153ce06c6
staybreeze <wunshengliao@gmail.com>
2024-01-27 17:28:11 +0800

    feat:修改後台進站總人數之顯示

--- back/total.php
+++ back/total.php
@@ -5,7 +5,7 @@
         <table style="width:50%;margin:auto">
             <tbody>
                 <tr class="yel">
-                    <td width="50%">網站標題</td>
+                    <td width="50%">進站總人數</td>
                     <td width="50%">
                         <input type="number" name="total" value="<?=$Total->find(1)['total'];?>">
                         <input type="hidden" name="table" value="<?=$do;?>">
